6c219f668f17ff1c5fe0be8039919fd64e401a6e
samudg7 <samudg7@users.noreply.github.com>
2022-11-17 00:23:40 +0100

    95bdcbae

---
+++ _nuxt/static/1668641015/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Fnews\u002F1","\u002Fclasses","\u002Fhome","\u002Fshows","\u002Fcontacts","\u002Fnews\u002F2","\u002Fstoria","\u002Fteachers","\u002Fnews\u002F3"]})
---
+++ _nuxt/static/1668641015/shows/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/shows", {data:[{}],fetch:{},mutations:void 0});
